d05a65c6cd6900045a0d2402a2fee9b5355a9d13
talha-rasheed-ada <talha.rasheed@ada.ac.uk>
2024-10-23 09:31:19 +0000

    Main Skeleton with rough comments, 2 Passes

--- dfp.js
+++ dfp.js
@@ -1,7 +1,48 @@
 const fs = require('fs');
 
 function parseFile (indata, outdata, delimiter = ';') {
+  
+  if (!fs.existsSync(indata)) {
+    return -1;
+  }
 
+  //Delete the output file 
+  if (fs.existsSync(outdata)) {
+    fs.unlinkSync(outdata);
+  }
+
+  //Read input file 
+  const fileContent = fs.readFileSync(indata, 'utf-8')
+
+  //Split the input file into different lines
+  const lines = fileContent.split(/\n/)
+
+  //Count number of records
+  let recordCount = 0
+
+  //Run through each line and trim
+  for (let i = 1; i < lines.length; i++) {
+    if (lines[i].trim() === ''){
+      continue;
+    }
+
+  //Split it into 2 columns, sentiment and review
+  let column = lines[i].split(delimiter)
+  let sentiment = column[0].trim()
+  let review = column[1].trim()
+
+  //Shorten review to 20 characters
+  review = review.substring(0,20)
+
+  //Add new line to output file using append
+  let newColumn = `${sentiment}${delimiter}${review}\n`
+  fs.appendFileSync(outdata, newColumn, 'utf-8');
+  recordCount++ ;
+
+  //return total number of records
+  return recordCount;
+  }
+  //Error checks
 }
 
 
